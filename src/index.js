f8abe11c4e76f6ad2b645fd3230413f9a3319342
Johnny Hauser <johnnyhauser@gmail.com>
2022-12-02 12:00:58 -0600

    ensure path starts with ..?/, otherwise a glob for the same directory as the importer will appear as a node_module import to node

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import fast_glob from 'fast-glob'
 
 export const virtual_module_id_prefix = 'virtual:glob:'
 
+const relative_path_regex = /^\.?\.\//
+const qualify_relative_path = x => relative_path_regex.test(x)
+
 export const create_module_for_glob = async ({ glob, importer }) => {
 	const files = await fast_glob(glob, { cwd: dirname(importer) })
 	return [
-		...files.map((file, index) => `import * as _${index} from '${file}'`),
+		...files.map((file, index) => `import * as _${index} from '${qualify_relative_path(file)}'`),
 		`export default {`,
 		files.map((file, index) => `  '${file}': _${index}`).join(',\n'),
 		`}`

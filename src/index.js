db50dece87dc5da3bb2346eaad024033db4a1ec6
Johnny Hauser <johnnyhauser@gmail.com>
2022-12-02 12:28:02 -0600

    finish the fix meant to be in the former commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import fast_glob from 'fast-glob'
 export const virtual_module_id_prefix = 'virtual:glob:'
 
 const relative_path_regex = /^\.?\.\//
-const qualify_relative_path = x => relative_path_regex.test(x)
+const qualify_relative_path = x => relative_path_regex.test(x) ? x : `./${x}`
 
 export const create_module_for_glob = async ({ glob, importer }) => {
 	const files = await fast_glob(glob, { cwd: dirname(importer) })
